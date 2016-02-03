c4a3b8254569f6e4259d9fba6fc6d22e13382fbd
yukiasai <yukiasai@gmail.com>
2016-02-03 18:36:45 +0900

    Add Minamo.h

---
+++ Minamo/Minamo.h
@@ -0,0 +1,17 @@
+//
+//  Minamo.h
+//  Minamo
+//
+//  Created by yukiasai on 2016/02/03.
+//  Copyright (c) 2016 yukiasai. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Minamo.
+FOUNDATION_EXPORT double MinamoVersionNumber;
+
+//! Project version string for Minamo.
+FOUNDATION_EXPORT const unsigned char MinamoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Minamo/PublicHeader.h>
